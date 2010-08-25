55592021898b1ddb510b41d4700c8d27796a368e
federico reiven <reiven@gmail.com>
2010-08-25 17:14:09 -0300

    modes now support the new auth method

--- modules/module_modes.py
+++ modules/module_modes.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+# this module handle all the commands related to mode / topic changes
+
+import hashlib
+from pysqlite2 import dbapi2 as sqlite3
+
 ##################################################################
 
 def handle_modeChanged(bot, user, channel, set, modes, args):
@@ -11,26 +16,20 @@ def handle_modeChanged(bot, user, channel, set, modes, args):
 
 ##################################################################
 
-def handle_topicUpdated(bot, user, channel, newtopic):
-
-    bot.log ('%s chaged topic to %s on %s' % (getNick(user),newtopic,channel))
-
-##################################################################
-
 def privcommand_topic(bot, user, channel, args):
     """Usage: !topic #channel (new topic message)"""
 
-    if args: 
-	if channel in bot.network.channels:
-	    bot.topic(channel,args)
-	    bot.say(user, 'ok, topic changed on %s' % channel)
+    if bot.checkValidHostmask(user):
+	if args: 
+	    if channel in bot.network.channels:
+		bot.topic(channel,args)
+		bot.say(user, 'ok, topic changed on %s' % channel)
 
-        else:
-	    bot.say(channel, 'sorry, i dont know that channel ')
+    	    else:
+		bot.say(channel, 'sorry, i dont know that channel ')
 
-
-    else:
-        bot.say(channel, "Usage error.  See 'help names'")
+	else:
+    	    bot.say(channel, "Usage error.  See 'help names'")
 
 
 ##################################################################
@@ -38,63 +37,62 @@ def privcommand_topic(bot, user, channel, args):
 def privcommand_names(bot, user, channel, args):
     """Show users in #channel"""
 
-    if args: 
-	if args in bot.network.channels:
-	    users = bot.getNames(args)
-	    for u in users:
-		bot.say(channel,"%s" % (u))
+    if bot.checkValidHostmask(user):
+	if args: 
+	    if args in bot.network.channels:
+		users = bot.getNames(args)
+		for u in users:
+		    bot.say(channel,"%s" % (u))
 
-        else:
-	    bot.say(channel, 'sorry, i dont know that channel ')
+    	    else:
+		bot.say(channel, 'sorry, i dont know that channel ')
 
-
-    else:
-        bot.say(channel, "Usage error.  See 'help names'")
-
-##################################################################
-
-def privcommand_isopped (bot, user, channel, args):
-
-    opped = '@' + getNick(user)
-
-    if opped in users:
-	bot.say(channel,"you are opped %s" % getNick(user))
-    else:
-	bot.say(channel,"not opped")
+	else:
+    	    bot.say(channel, "Usage error.  See 'help names'")
 
 ##################################################################
 
 def privcommand_op(bot, user, channel, args):
     """!op (password) user channel - Mode +o the specified user"""
 
-    if len(args.split()) == 3:
-	password, toop , w2o = args.split()
-	if password == "password":
+    if bot.checkValidHostmask(user):
+        if len(args.split()) == 3:
+            passwd,toop,w2o = args.split()
 	    if w2o in bot.network.channels:
-
-		bot.say(channel, '%s requested op for %s on %s' % (getNick(user),toop,w2o))
-    		bot.mode(w2o, 1, 'o', user=toop)
+        	passwd = hashlib.sha1(passwd).hexdigest()
+        	conn = sqlite3.connect(bot.nickname + ".db")
+        	cursor = conn.cursor()
+        	cursor.execute("SELECT name FROM users where passwd = '%s'" % passwd)
+        	comp = cursor.fetchone()
+        	if comp:
+		    bot.log('%s requested op for %s on %s' % (str(comp[0]),toop,w2o))
+    		    bot.mode(w2o, 1, 'o', user=toop)
 
     	    else:
 		bot.say(channel, 'sorry, i dont know that channel ')
 
-    else:
-        bot.say(channel, "Usage error.  See 'help op'")
-
+	else:
+    	    bot.say(channel, "Usage error.  See 'help op'")
 
 def privcommand_deop(bot, user, channel, args):
-    """Usage: deop (password) username - Mode -o the specified user"""
+    """Usage: deop (password) user channel - Mode -o the specified user"""
 
-    if len(args.split()) == 3:
-	password, deop , w2d= args.split()
-	if password == "password":
-	    if w2d in bot.network.channels:
-		bot.say(channel, '%s requested deop for %s on %s' % (getNick(user),deop,w2d))
-    		bot.mode(w2d, 0, 'o', user=deop)
+    if bot.checkValidHostmask(user):
+        if len(args.split()) == 3:
+            passwd,deop,w2o = args.split()
+	    if w2o in bot.network.channels:
+        	passwd = hashlib.sha1(passwd).hexdigest()
+        	conn = sqlite3.connect(bot.nickname + ".db")
+        	cursor = conn.cursor()
+        	cursor.execute("SELECT name FROM users where passwd = '%s'" % passwd)
+        	comp = cursor.fetchone()
+        	if comp:
+		    bot.log('%s requested deop for %s on %s' % (str(comp[0]),deop,w2o))
+    		    bot.mode(w2o, 0, 'o', user=deop)
 
     	    else:
-		bot.say(channel, 'sorry, i dont know that chanel ')
+		bot.say(channel, 'sorry, i dont know that channel ')
 
-    else:
-        bot.say(channel, "Usage error.  See 'help op'")
+	else:
+    	    bot.say(channel, "Usage error.  See 'help deop'")
 
