39ca61acc241dc1b5b756e8ad70925b9c002bd14
federico reiven <reiven@gmail.com>
2010-08-25 13:16:31 -0300

    new autoop module

--- modules/module_autoop.py
+++ modules/module_autoop.py
@@ -3,7 +3,7 @@ def handle_userJoined(bot, user, channel):
     """ only auto-op bot and masters"""
 
     lvl = bot.checkValidHostmask(user)
-    if lvl > 4:
+    if lvl and lvl >= 4:
         bot.log("auto-opping %s" % user)
         bot.mode(channel, True, 'o', user=getNick(user))
 
