327cbbd7b8042ff92322135d862f2de8211e2bea
federico reiven <reiven@gmail.com>
2010-08-26 16:59:55 -0300

    !delbot added

--- modules/module_auth.py
+++ modules/module_auth.py
@@ -4,6 +4,9 @@ from pysqlite2 import dbapi2 as sqlite3
 import hashlib
 import string
 
+######################################################################
+# auth : authenticate to the bot. this will auto-update your hostmask
+
 def privcommand_auth(bot, user, channel, args):
     """Usage: !auth user password """
 
@@ -32,6 +35,9 @@ def privcommand_auth(bot, user, channel, args):
     else:
         bot.say(channel, "Usage error.  See 'help auth'")
 
+######################################################################
+# adduser : this will add a new user to the db, setting him a password
+
 def privcommand_adduser(bot,user,channel,args):
     """Usage: !useradd user password"""
 
@@ -49,8 +55,12 @@ def privcommand_adduser(bot,user,channel,args):
 	else:
     	    bot.say(channel, "Usage error.  See 'help adduser'")
 
+######################################################################
+# addbot : the main difference between users and bots is lack of password (by now) 
+#          for the bots, and the hostmask need to be precise to auto-op them
+
 def privcommand_addbot(bot,user,channel,args):
-    """Usage: !userbot botname hostmask"""
+    """Usage: !addbot botname hostmask"""
 
     lvl = bot.checkValidHostmask(user)
     if lvl and lvl >= 3:
@@ -66,7 +76,35 @@ def privcommand_addbot(bot,user,channel,args):
 	else:
     	    bot.say(channel, "Usage error.  See 'help addbot'")
 
-### set *
+def privcommand_delbot(bot,user,channel,args):
+    """Usage: !delbot [password] botname"""
+
+    lvl = bot.checkValidHostmask(user)
+    if lvl and lvl >= 3:
+	if len(args.split()) == 2:
+	    passwd, botname = args.split()
+	    passwd = hashlib.sha1(passwd).hexdigest()
+	    conn = sqlite3.connect(bot.nickname + ".db")
+	    cursor = conn.cursor()
+	    cursor.execute("SELECT name FROM users where passwd = '%s'" % passwd)
+    	    comp = cursor.fetchone()
+            if comp:
+	        cursor.execute("SELECT name,level,hostmask FROM users WHERE name = '%s'" % botname)
+		dele = cursor.fetchone()
+		if dele and str(dele[1]) == "4":
+	    	    cursor.execute("DELETE FROM users WHERE name = '%s'" % botname)
+		    conn.commit()
+		    del bot.authenticated[str(dele[2])]
+		    bot.say(channel, "%s deleted ok" % (botname.lower()))
+		else:
+		    bot.say(channel, "%s is not a valid bot" % (botname.lower()))
+
+	else:
+    	    bot.say(channel, "Usage error.  See 'help delbot'")
+
+######################################################################
+# set: [host]  change hostmask (allowing you use wildcards)
+#      [email] set email
 
 def privcommand_sethost(bot, user, channel, args):
     """Usage: !sethost password newhostmask """
@@ -92,7 +130,8 @@ def privcommand_sethost(bot, user, channel, args):
     else:
 	bot.say(channel, "sorry, u're not authenticated yet")
 
-### check *
+######################################################################
+### check : commands for verification of auto-auth
 
 def privcommand_check(bot, user, channel, args):
     """Usage: !check [auth/level] """
