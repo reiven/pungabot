fc69b76ad66f7a49319dd4ad46f5bf1c69e4c2b2
federico reiven <reiven@gmail.com>
2010-08-26 13:10:50 -0300

    new auth commands

--- modules/module_auth.py
+++ modules/module_auth.py
@@ -31,7 +31,7 @@ def privcommand_auth(bot, user, channel, args):
     else:
         bot.say(channel, "Usage error.  See 'help auth'")
 
-def privcommand_useradd(bot,user,channel,args):
+def privcommand_adduser(bot,user,channel,args):
     """Usage: !useradd user password"""
 
     lvl = bot.checkValidHostmask(user)
@@ -45,13 +45,30 @@ def privcommand_useradd(bot,user,channel,args):
 	    bot.say(channel, "%s added with passwd (%s) " % (user.lower(),password))
 
 	else:
-    	    bot.say(channel, "Usage error.  See 'help useradd'")
+    	    bot.say(channel, "Usage error.  See 'help adduser'")
+
+def privcommand_addbot(bot,user,channel,args):
+    """Usage: !userbot botname hostmask"""
+
+    lvl = bot.checkValidHostmask(user)
+    if lvl and lvl >= 3:
+	if len(args.split()) == 2:
+	    botname,hostmask = args.split()
+	    conn = sqlite3.connect(bot.nickname + ".db")
+	    cursor = conn.cursor()
+	    cursor.execute("INSERT INTO users VALUES (NULL,?,NULL,'4',?,NULL)",(unicode(botname.lower(),'utf-8'),hostmask))
+	    conn.commit()
+	    bot.say(channel, "%s added with hostmask (%s) " % (botname.lower(),hostmask))
+
+	else:
+    	    bot.say(channel, "Usage error.  See 'help addbot'")
+
+### set *
 
 def privcommand_sethost(bot, user, channel, args):
     """Usage: !sethost password newhostmask """
 
-    lvl = bot.checkValidHostmask(user)
-    if lvl:
+    if bot.checkValidHostmask(user):
 	if len(args.split()) == 2:
 	    passwd,newhm = args.split()
 	    passwd = hashlib.sha1(passwd).hexdigest()
@@ -71,11 +88,18 @@ def privcommand_sethost(bot, user, channel, args):
     else:
 	bot.say(channel, "sorry, u're not authenticated yet")
 
-def privcommand_checkauth(bot, user, channel, args):
-    """Usage: !checkauth """
+### check *
 
-    lvl = bot.checkValidHostmask(user)
-    if lvl:
-	bot.say(channel, "authenticated with level %s" % lvl)
+def privcommand_check(bot, user, channel, args):
+    """Usage: !check [auth/level] """
+
+    if args and bot.checkValidHostmask(user):
+        if args.lower() == "auth":
+	    if lvl:
+		bot.say(channel, "you are authenticated ok")
+
+        if args.lower() == "level":
+	    if lvl:
+		bot.say(channel, "your actual level is %s" % lvl)
     else:
-	bot.say(channel, "sorry, u're not authenticated yet")
+        bot.say(channel, "wrong usage, see help")
