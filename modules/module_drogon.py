9ae51d5c1767d624e15d221e8a14539b60cf1786
federico reiven <reiven@gmail.com>
2010-08-27 16:35:21 -0300

    only a chance of 30%

--- modules/module_drogon.py
+++ modules/module_drogon.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import re
+import random
 
 def handle_privmsg (bot, user, channel, message):
 
-    MSG = ['papel' ,'merca' ,'blanca', 'cameruza', 'tripa','pepa','trip',
-	    'porro','faso','churro','maconia','fumar','volar']
+    ono = random.randrange(10)
+    # we have a chances of 30% of use this module
+    if ono < 7:
+        return
+
+    MSG = ['papel' ,'merca' ,'blanca', 'cameruza', 'pala','pase','toque',
+            'pasta','bicho',
+            'tripa','pepa','trip','carton','gotero','gota',
+            'porro','faso','churro','maconia','fumar','volar']
     regex = re.compile('(' + '|'.join(MSG) + ')', re.IGNORECASE)
 
     if regex.findall(message):
