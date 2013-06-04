5769a599ca626c198551258d0755730f0ef0e588
reiven <reiven@gmail.com>
2013-06-04 13:40:44 -0300

    updated riesgopais inside dolar module

--- modules/module_dolar.py
+++ modules/module_dolar.py
@@ -1,11 +1,11 @@
-import urllib2
+from urllib2 import urlopen
 from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup
 
 
 def command_dolar(bot, user, channel, args):
     """show the dollar exchange in .ar"""
 
-    page = urllib2.urlopen("http://www.cotizacion-dolar.com.ar/")
+    page = urlopen("http://www.cotizacion-dolar.com.ar/")
     soup = BeautifulSoup(page, convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
     tabla = soup.findAll("td", {"class": "cotizaciones3"})
     bot.say(channel, '%s compra - %s venta' % (
@@ -18,7 +18,7 @@ def command_dolar(bot, user, channel, args):
 def command_euro(bot, user, channel, args):
     """show the euro exchange in .ar"""
 
-    page = urllib2.urlopen("http://www.cotizacion-dolar.com.ar/")
+    page = urlopen("http://www.cotizacion-dolar.com.ar/")
     soup = BeautifulSoup(page, convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
     tabla = soup.findAll("td", {"class": "cotizaciones3"})
     bot.say(channel, '%s compra - %s venta' % (
@@ -31,10 +31,12 @@ def command_euro(bot, user, channel, args):
 def command_riesgopais(bot, user, channel, args):
     """show current argentina's country risk index"""
 
-    page = urllib2.urlopen("http://www.riesgopais.com")
+    page = urlopen("http://www.ambito.com/economia/mercados/riesgo-pais/info/?id=2")
     soup = BeautifulSoup(page, convertEntities=BeautifulStoneSoup.HTML_ENTITIES)
-    tabla = soup.findAll("table", {"width": "480"})
-    datos = tabla[0].find('td', attrs={'valign': 'top'})
-    final = unicode(datos.findAll(text=True)).split("\\")
-    bot.say(channel, '%s' % final[4].split('n')[1].encode("utf-8"))
+    valor = soup.findAll("div", {"id": "ultimo"})
+    variacion = soup.findAll("div", {"id": "variacion"})
+    bot.say(channel, '%s (%s)' % (
+        valor[0].findAll("big")[0].renderContents(),
+        variacion[0].findAll("big")[0].renderContents()
+    ))
     return
